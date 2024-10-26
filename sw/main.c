954f4ceab4ff839046c38329919f3572921fb89a
Joel Vandergriendt <joelvandergriendt@microridge.ca>
2024-10-26 14:31:46 -0700

    software mostly works except for debouncing

--- sw/main.c
+++ sw/main.c
@@ -9,7 +9,8 @@
 #include "stdlib.h"
 
 //currently changes target timing and serial reports
-#define dg_printf(...) do{uartEnable(); _delay_ms(100);printf(__VA_ARGS__ ); _delay_ms(100);uartDisable();}while(0);
+//#define dg_printf(...) do{uartEnable(); _delay_ms(100);printf(__VA_ARGS__ ); _delay_ms(100);uartDisable();}while(0);
+#define dg_printf(...)
 #define debugx(x) dg_printf(#x "=0x%x\r\n",(unsigned int)(x));
 #define debug(x) dg_printf(#x "=%d\r\n",(unsigned int)(x));
 
@@ -41,24 +42,21 @@ inline static __attribute__((always_inline)) void wdt_disable() {
 #define OUT_LOCK_R   PB2
 
 void setup_pins(){
-    PUEA =  _BV(SW_DRV_SEAT) | _BV(SW_PAS_SEAT) | _BV(SW_LIGHTS) | _BV(SW_LOCK_F) | _BV(SW_LOCK_R);
-    //ports are set to inputs. output pins are externally pulled high.
-    //when we want to output a 1, we write 1 to ddr pin
-    PORTA = 0;
-    PORTB = 0;
-    DDRA = _BV(BACKLIGHT_EN);
-    DDRB = 0;
+    PUEA = _BV(SW_DRV_SEAT) | _BV(SW_PAS_SEAT) | _BV(SW_LIGHTS) | _BV(SW_LOCK_F) | _BV(SW_LOCK_R);
+    PUEB = 0;
+
+    PORTA = _BV(BACKLIGHT_EN) | _BV(OUT_DRV_SEAT) | _BV(OUT_LOCK_F);
+    PORTB = _BV(OUT_PAS_SEAT) | _BV(OUT_LIGHTS) | _BV(OUT_LOCK_R);
+    DDRA = _BV(BACKLIGHT_EN) | _BV(OUT_DRV_SEAT) | _BV(OUT_LOCK_F);
+    DDRB = _BV(OUT_PAS_SEAT) | _BV(OUT_LIGHTS) | _BV(OUT_LOCK_R);
 
 }
-typedef union{
-    struct {
-        uint8_t drv_seat : 1;
-	uint8_t pas_seat :1;
-	uint8_t lights:1;
-	uint8_t lock_f:1;
-	uint8_t lock_r:1;
-    };
-    uint8_t as_int;
+typedef struct {
+    uint8_t drv_seat ;
+    uint8_t pas_seat ;
+    uint8_t lights;
+    uint8_t lock_f;
+    uint8_t lock_r;
 }switches_t;
 
 switches_t get_sw(){
@@ -73,11 +71,8 @@ switches_t get_sw(){
 
 void set_outputs(switches_t state){
 
-    uint8_t old_a = DDRA & ~(_BV(OUT_DRV_SEAT)  | _BV(OUT_LOCK_F));
-    DDRA = old_a | ( state.drv_seat << OUT_DRV_SEAT) | (state.lock_f << OUT_LOCK_F);
-
-    uint8_t old_b = DDRB & ~(_BV(OUT_LIGHTS)  | _BV(OUT_PAS_SEAT) | _BV(OUT_LOCK_R));
-    DDRB = old_b | ( state.lights << OUT_LIGHTS) | (state.lock_f << OUT_PAS_SEAT)| (state.lock_r << OUT_LOCK_R);
+    PORTA = ( state.drv_seat << OUT_DRV_SEAT) | (state.lock_f << OUT_LOCK_F)| _BV(BACKLIGHT_EN);
+    PORTB = ( state.lights << OUT_LIGHTS) | (state.pas_seat << OUT_PAS_SEAT)| (state.lock_r << OUT_LOCK_R) ;
 
 }
 void delay_us(int us){
@@ -98,10 +93,18 @@ void pwm_delayms(int ms,int dutycycle){
 	}
     }
 }
+void blink_slow(int blinks){
+    while(blinks--){
+    PORTA &= ~_BV(BACKLIGHT_EN);
+    _delay_ms(500);
+    PORTA|=_BV(BACKLIGHT_EN);
+    _delay_ms(500);
+    }
+}
 
 void pwm_test(){
     int pwm = 10;
-    int test=100;
+    int test=1000;
     while(--test){
 	//dg_printf("pwm = %d\r\n",pwm);
 	pwm_delayms(1000,pwm);
@@ -115,6 +118,7 @@ void pwm_test(){
 	_delay_ms(1000);
     }
 }
+#define DEBOUNCE_COUNT 255
 int main (void) {
 // MAIN STARTUP CODE:
 
@@ -123,45 +127,66 @@ int main (void) {
     int i=0;
     setup_pins();
 
-    switches_t state;
-    state.as_int=0;
+    switches_t state = {0};
     set_outputs(state);
     switches_t last_sw=get_sw();
+    switches_t debounce_count = {0};
     int32_t loop_count=0;
-    pwm_test();
+    //pwm_test();
+    blink_slow(4);
+
+
     while (1) {
 	switches_t sw= get_sw();
 
 	//look for falling edges
-	if(last_sw.drv_seat && !sw.drv_seat ){
+	if(last_sw.drv_seat && !sw.drv_seat && debounce_count.drv_seat == 0){
 	    dg_printf("drv_seat pressed\r\n");
 	    state.drv_seat = !state.drv_seat;
+	    debounce_count.drv_seat = DEBOUNCE_COUNT;
 	}
 
-	if(last_sw.pas_seat && !sw.pas_seat ){
+	if(last_sw.pas_seat && !sw.pas_seat && debounce_count.pas_seat == 0){
 	    dg_printf("pas_seat pressed\r\n");
-	    state.pas_seat = !state.pas_seat;
+            state.pas_seat = !state.pas_seat;
+	    debounce_count.pas_seat = DEBOUNCE_COUNT;
 	}
 
-	if(last_sw.lights && !sw.lights ){
+	if(last_sw.lights && !sw.lights && debounce_count.lights == 0){
 	    dg_printf("lights pressed\r\n");
-	    state.lights = !state.lights;
+            state.lights = !state.lights;
+	    debounce_count.lights = DEBOUNCE_COUNT;
 	}
-	if(last_sw.lock_f && !sw.lock_f ){
+	if(last_sw.lock_f && !sw.lock_f && debounce_count.lock_f == 0){
 	    dg_printf("lock_f pressed\r\n");
-	    state.lock_f = !state.lock_f;
+            state.lock_f = !state.lock_f;
+	    debounce_count.lock_f = DEBOUNCE_COUNT;
 	}
-	if(last_sw.lock_r && !sw.lock_r ){
+	if(last_sw.lock_r && !sw.lock_r && debounce_count.lock_r == 0){
 	    dg_printf("lock_r pressed\r\n");
-	    state.lock_r = !state.lock_r;
+            state.lock_r = !state.lock_r;
+	    debounce_count.lock_r = DEBOUNCE_COUNT;
+	}
+
+        if (debounce_count.drv_seat){
+            debounce_count.drv_seat--;
+	}
+	if (debounce_count.pas_seat){
+            debounce_count.pas_seat--;
+	}
+	if (debounce_count.lights){
+            debounce_count.lights--;
+	}
+        if (debounce_count.lock_r){
+            debounce_count.lock_r--;
+	}
+	if(debounce_count.lock_f){
+	    debounce_count.lock_f--;
 	}
 
 	last_sw = sw;
 	set_outputs(state);
 	loop_count++;
-	if((loop_count&1023) == 0){
-	    dg_printf("hello world %d %x %x\r\n",i++,state.as_int,get_sw().as_int);
-	}
 
     }
     return 0;
