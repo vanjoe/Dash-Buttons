4d71bc37a24bbcf3456acf621e38d5074a761c92
Joel Vandergriendt <joelvandergriendt@microridge.ca>
2024-10-28 08:18:05 -0700

    debounce sw works

--- sw/main.c
+++ sw/main.c
@@ -51,12 +51,15 @@ void setup_pins(){
     DDRB = _BV(OUT_PAS_SEAT) | _BV(OUT_LIGHTS) | _BV(OUT_LOCK_R);
 
 }
-typedef struct {
+typedef union{
+ struct {
     uint8_t drv_seat ;
     uint8_t pas_seat ;
     uint8_t lights;
     uint8_t lock_f;
     uint8_t lock_r;
+ };
+ uint8_t as_array[5];
 }switches_t;
 
 switches_t get_sw(){
@@ -93,12 +96,27 @@ void pwm_delayms(int ms,int dutycycle){
 	}
     }
 }
+int set_backlight(int en){
+    if(en){
+	PORTA|=_BV(BACKLIGHT_EN);
+    }else{
+	PORTA &= ~_BV(BACKLIGHT_EN);
+    }
+}
 void blink_slow(int blinks){
     while(blinks--){
-    PORTA &= ~_BV(BACKLIGHT_EN);
-    _delay_ms(500);
-    PORTA|=_BV(BACKLIGHT_EN);
-    _delay_ms(500);
+	set_backlight(0);
+	_delay_ms(500);
+	set_backlight(1);
+	_delay_ms(500);
+    }
+}
+void blink_fast(int blinks){
+    while(blinks--){
+	set_backlight(0);
+	_delay_ms(100);
+	set_backlight(1);
+	_delay_ms(100);
     }
 }
 
@@ -119,74 +137,43 @@ void pwm_test(){
     }
 }
 #define DEBOUNCE_COUNT 255
+
+void debounce_falling(uint8_t* debounce,uint8_t sw){
+    if(sw) {
+	*debounce = DEBOUNCE_COUNT;
+    } else if(*debounce){
+	*debounce = *debounce-1;
+    }
+}
 int main (void) {
 // MAIN STARTUP CODE:
 
     wdt_disable();
     sei();
-    int i=0;
+
     setup_pins();
 
     switches_t state = {0};
     set_outputs(state);
-    switches_t last_sw=get_sw();
+    switches_t last_debounce_count={0};
     switches_t debounce_count = {0};
-    int32_t loop_count=0;
-    //pwm_test();
-    blink_slow(4);
 
+    //pwm_test();
+    //blink_slow(4);
+    blink_fast(4);
 
     while (1) {
 	switches_t sw= get_sw();
 
-	//look for falling edges
-	if(last_sw.drv_seat && !sw.drv_seat && debounce_count.drv_seat == 0){
-	    dg_printf("drv_seat pressed\r\n");
-	    state.drv_seat = !state.drv_seat;
-	    debounce_count.drv_seat = DEBOUNCE_COUNT;
+	for(int s=0;s<sizeof(sw.as_array);++s){
+	    //debounce look for falling edges
+	    debounce_falling(&debounce_count.as_array[s], sw.as_array[s]);
+	    if(debounce_count.as_array[s] == 0 && last_debounce_count.as_array[s] == 1){
+		state.as_array[s] = !state.as_array[s];
+	    }
 	}
-
-	if(last_sw.pas_seat && !sw.pas_seat && debounce_count.pas_seat == 0){
-	    dg_printf("pas_seat pressed\r\n");
-            state.pas_seat = !state.pas_seat;
-	    debounce_count.pas_seat = DEBOUNCE_COUNT;
-	}
-
-	if(last_sw.lights && !sw.lights && debounce_count.lights == 0){
-	    dg_printf("lights pressed\r\n");
-            state.lights = !state.lights;
-	    debounce_count.lights = DEBOUNCE_COUNT;
-	}
-	if(last_sw.lock_f && !sw.lock_f && debounce_count.lock_f == 0){
-	    dg_printf("lock_f pressed\r\n");
-            state.lock_f = !state.lock_f;
-	    debounce_count.lock_f = DEBOUNCE_COUNT;
-	}
-	if(last_sw.lock_r && !sw.lock_r && debounce_count.lock_r == 0){
-	    dg_printf("lock_r pressed\r\n");
-            state.lock_r = !state.lock_r;
-	    debounce_count.lock_r = DEBOUNCE_COUNT;
-	}
-
-        if (debounce_count.drv_seat){
-            debounce_count.drv_seat--;
-	}
-	if (debounce_count.pas_seat){
-            debounce_count.pas_seat--;
-	}
-	if (debounce_count.lights){
-            debounce_count.lights--;
-	}
-        if (debounce_count.lock_r){
-            debounce_count.lock_r--;
-	}
-	if(debounce_count.lock_f){
-	    debounce_count.lock_f--;
-	}
-
-	last_sw = sw;
+	last_debounce_count = debounce_count;
 	set_outputs(state);
-	loop_count++;
 
     }
     return 0;
